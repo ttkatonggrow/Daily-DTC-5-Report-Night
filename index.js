7ace4dc5b361bb08ad2e22ba68dd2635b1565157
ttkatonggrow <ttkatonggrow@gmail.com>
2026-02-27 21:48:35 +0700

    Update index.js

--- index.js
+++ index.js
@@ -361,8 +361,8 @@ function zipFiles(sourceDir, outPath, filesToZip) {
         }, startDateTime, endDateTime);
         
         await page.click('td:nth-of-type(6) > span');
-        console.log('   ⏳ Waiting 4 mins (Strict)...');
-        await new Promise(r => setTimeout(r, 240000));
+        console.log('   ⏳ Waiting 4.5 mins (Strict)...');
+        await new Promise(r => setTimeout(r, 270000));
         await page.evaluate(() => document.getElementById('btnexport').click());
         const file2 = await waitForDownloadAndRename(downloadPath, 'Report2_Idling.xls');
 
